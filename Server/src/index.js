bec2cee710f53c02987778101ca7bf8af6c016d3
viri <viryubaldomendoza@gmail.com>
2023-11-10 22:26:16 -0600

    done

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,9 +1,15 @@
+const { conn } = require('./DB_connection');
 const server = require('./app');
+
 const PORT = 3001;
 
+conn.sync({ force: true }).then(() => {
 server.listen(PORT, () => {
-    console.log('Server raised in port: ' + PORT);
+    console.log('Server raised in port: ' + PORT);    
+})
 });
+
+
 /*const express= require('express');
 const router = require('./routes/index');
 
