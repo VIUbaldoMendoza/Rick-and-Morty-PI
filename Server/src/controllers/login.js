3969b3c14d5dabd7180669ae08b6fc824b31440b
viri <viryubaldomendoza@gmail.com>
2023-11-14 19:55:28 -0600

    borrar .env

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,16 +1,46 @@
-const users = require("../utils/users");
+const { User } = require('../DB_connection')
 
-const login = (req, res) => {
-    const { email, password } = req.query;
-    let access = false;
-
-    users.forEach((user) => {
-        if(user.email === email && user.password === password) {
-            access = true;   
+const login = async( req, res) => {
+    try {
+        const { email, password } = req.query
+        if(!email || !password){
+            return res.status(400).send('Faltan datos')
         }
-    });
 
-    return res.json({ access });
+        const user = await User.findOne({
+            where: { email: email }
+        })
+
+        if (!user) return res.status(404). send('Usuario no encontrado.')
+
+        if(user.password === password){
+            return res.json({
+                access: true
+            })
+        } else {
+            return res.status(403).send('Constraseña incorrecta.')
+        }
+    } catch (error) {
+        return res.status(500).json(error.message)
+        
+    }
 }
 
 module.exports = login;
+//login antes de database
+// const users = require("../utils/users");
+
+// const login = (req, res) => {
+//     const { email, password } = req.query;
+//     let access = false;
+
+//     users.forEach((user) => {
+//         if(user.email === email && user.password === password) {
+//             access = true;   
+//         }
+//     });
+
+//     return res.json({ access });
+// }
+
+// module.exports = login;
--- Server/src/utils/users.js
+++
@@ -1,1 +0,0 @@
-module.exports = [{email: 'viri@gmail.com', password: '12345' }];
